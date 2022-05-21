fe0e1f83f91830c4a86dfcc64139d078330c6611
Muhammad Waleed <mujahidwaleed11@gmail.com>
2022-05-21 16:09:08 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { Routes, Route, Link } from "react-router-dom";
-import Navbar from "./Components/Navbar";
-import Landing from "./Pages/Landing";
-import SingleNftPage from "./Pages/SingleNftPage";
+import { Routes, Route, Link } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Landing from './Pages/Landing';
+import SingleNftPage from './Pages/SingleNftPage';
+import './App.css';
+
 function App() {
   return (
     <div>
-    
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="about" element={<SingleNftPage />} />
--- src/Components/NftCard.js
+++ src/Components/NftCard.js
@@ -1,41 +1,56 @@
-import React from "react";
-import { Box, Paper, Grid } from "@mui/material";
-import SilverNft from "../Assets/silverNft.png";
-import GoldenNft from "../Assets/goldenNft.png";
+import React from 'react';
+import { Box, Paper, Grid } from '@mui/material';
+import SilverNft from '../Assets/silverNft.png';
+import GoldenNft from '../Assets/goldenNft.png';
 
-import GreenNft from "../Assets/GreenNft.png";
+import GreenNft from '../Assets/GreenNft.png';
 
-import BlueNft from "../Assets/BlueNft.png";
-import RedNft from "../Assets/RedNft.png";
+import BlueNft from '../Assets/BlueNft.png';
+import RedNft from '../Assets/RedNft.png';
 
 export default function NftCard() {
   const NftArray = [SilverNft, GoldenNft, GreenNft, BlueNft, RedNft];
   return (
     <>
-      {NftArray.map((data) => {
+      {NftArray.map(data => {
         return (
           <Box
             sx={{ flexGrow: 1 }}
-            style={{ padding: "5%", backgroundColor: "silver" }}
-          >
+            className="main-nft-box"
+            style={{
+              padding: '5%',
+              position: 'relative',
+            }}>
+            <div
+              style={{
+                height: '100%',
+                width: '100%',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                backgroundColor: 'rgba(0,0,0,0.9)',
+              }}
+            />
             <Grid
               container
               spacing={2}
               // direction="column"
               // alignItems="center"
               // justifyContent="center"
-            >
+              style={{
+                position: 'relative',
+                zIndex: 999,
+              }}>
               <Grid
                 item
                 xs={12}
                 md={5}
                 spacing={2}
                 style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
                 <img src={data} width="70%" />
               </Grid>
               <Grid
@@ -48,16 +63,17 @@ export default function NftCard() {
                     // justifyContent: "center",
                     // alignItems: "center",
                   }
-                }
-              >
+                }>
                 <div
                   style={{
-                    display: "flex",
-                    flexDirection: "column",
+                    display: 'flex',
+                    flexDirection: 'column',
                     //   justifyContent: "left",
                     //   alignItems: "center",
-                  }}
-                >
+                    position: 'relative',
+                    zIndex: 999,
+                    color: 'white',
+                  }}>
                   <h2 className="newFont">NFT MINT SECTION</h2>
                   <p>
                     Loirm ipsam is a collection of 10,000 unique, randomly
@@ -65,7 +81,7 @@ export default function NftCard() {
                     ERC-721 NFTs.
                   </p>
                   <p>
-                    {" "}
+                    {' '}
                     Every Angry Boar is unique and programmatically generated
                     from over 250+ possible attributes and traits like
                     background, skin, clothes, mouth, head, tusks, snout, eyes
@@ -73,21 +89,35 @@ export default function NftCard() {
                   </p>
                   <div
                     style={{
-                      display: "flex",
+                      display: 'flex',
 
-                      alignItems: "center",
-                    }}
-                  >
-                    <p>Price:</p> <p>29.00 ETH</p>
+                      alignItems: 'center',
+                    }}>
+                    <p>Price:</p>{' '}
+                    <p
+                      style={{
+                        fontFamily: 'Space Ranger',
+                        marginLeft: 10,
+                        fontSize: 24,
+                      }}>
+                      29.00 ETH
+                    </p>
                   </div>
                   <div
                     style={{
-                      display: "flex",
+                      display: 'flex',
 
-                      alignItems: "center",
-                    }}
-                  >
-                    <p>Date:</p> <p>29.12.2023</p>
+                      alignItems: 'center',
+                    }}>
+                    <p>Date:</p>{' '}
+                    <p
+                      style={{
+                        fontFamily: 'Space Ranger',
+                        marginLeft: 10,
+                        fontSize: 24,
+                      }}>
+                      29.12.2023
+                    </p>
                   </div>
                 </div>
               </Grid>
